e4ba07945b8db12b7ac0ee7c685572207bc45f1c
sheppard <vivchar.danylo@gmail.com>
2014-09-18 19:54:43 +0300

    minor fixes

--- trunk/phpapp/public/javascripts/dropmain.js
+++ trunk/phpapp/public/javascripts/dropmain.js
@@ -1,17 +1,19 @@
 $("div#drop_zone").dropzone({
     url: "/upload.php",
     dictDefaultMessage: "",
-    init: function() {
-        this.on("addedfile", function(file) {
-            $('.fileUpload').css('display','inline-block');
+    init: function () {
+        this.on("addedfile", function (file) {
+            $('.fileUpload').css('display', 'inline-block');
+        });
+
+        this.on("success", function () {
+            console.log(arguments);
         });
     }
 });
 
-$('#drop_zone').on('dragenter',function(){
-    $('.fileUpload').css('display','none');
-});
-
-$('#drop_zone').on('dragleave',function(){
-    $('.fileUpload').css('display','inline-block');
+$('#drop_zone').on('dragenter', function () {
+    $('.fileUpload').css('display', 'none');
+}).on('dragleave', function () {
+    $('.fileUpload').css('display', 'inline-block');
 });
