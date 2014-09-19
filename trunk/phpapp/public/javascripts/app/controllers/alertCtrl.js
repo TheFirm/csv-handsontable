1be966a4dfc12e33344460bdb8e058d2e5845b29
sheppard <vivchar.danylo@gmail.com>
2014-09-19 13:34:20 +0300

    added errors display angular

--- trunk/phpapp/public/javascripts/app/controllers/alertCtrl.js
+++ trunk/phpapp/public/javascripts/app/controllers/alertCtrl.js
@@ -11,12 +11,26 @@ APP.controller('alertCtrl', function($scope, $rootScope, $route, $routeParams) {
         if(data.success){
             $scope.message = {
                 error: false,
-                text: "All ok"
+                text: "Validation successful. You may import data."
             };
         } else {
+            var errorText = 'Error';
+            console.log(data.errors.bestMatch);
+            var bestMatch = data.errors.bestMatch;
+            var configName = Object.keys(bestMatch)[0];
+            var errors = bestMatch[configName];
+
+            if(errors.from_file.length === 0 && errors.from_conf.length !== 0){
+                errorText = 'You are missing columns: ' + errors.from_conf.join(', ')
+            } else if(errors.from_file.length !== 0 && errors.from_conf.length === 0){
+                errorText = 'You have to much columns: ' + errors.from_file.join(', ')
+            } else if(errors.from_file.length !== 0 && errors.from_conf.length !== 0){
+                errorText = 'You have errors columns: ' + errors.from_file.join(', ')
+            }
+
             $scope.message = {
                 error: true,
-                text: "Invalid data (!!!todo column names, or numbers)"
+                text: errorText
             };
         }
     });
