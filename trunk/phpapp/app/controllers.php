00819bf691da81def44246ef1a83827cf7c2ddb2
sheppard <vivchar.danylo@gmail.com>
2014-09-19 17:59:59 +0300

    x-editable test

--- trunk/phpapp/app/controllers.php
+++ trunk/phpapp/app/controllers.php
@@ -8,23 +8,23 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
 function authorize(){
-    $api = new Humanity\Api(array(
-        'client_id' => '95982517764489216',
-        'client_secret' => null,
-        'redirect_uri' => 'http://www.humanity.dev/php_sdk/oauth.php',
-    ));
-
-    // This is changed to match our endpoints
-    $api->setAuthorizeEndpoint('https://master-accounts.dev.humanity.com/oauth2/authorize');
-    $api->setTokenEndpoint('https://master-accounts.dev.humanity.com/oauth2/token');
-    $api->setApiEndpoint('https://master-api.dev.humanity.com/v1/');
-
-    if (!$api->hasAccessToken() && !$api->requestTokenWithAuthCode()) {
-    // No valid access token available, go to authorization server
-        header('Location: ' . $api->getAuthorizeUri());
-        exit;
-    }
-    return $api;
+//    $api = new Humanity\Api(array(
+//        'client_id' => '95982517764489216',
+//        'client_secret' => null,
+//        'redirect_uri' => 'http://www.humanity.dev/php_sdk/oauth.php',
+//    ));
+//
+//    // This is changed to match our endpoints
+//    $api->setAuthorizeEndpoint('https://master-accounts.dev.humanity.com/oauth2/authorize');
+//    $api->setTokenEndpoint('https://master-accounts.dev.humanity.com/oauth2/token');
+//    $api->setApiEndpoint('https://master-api.dev.humanity.com/v1/');
+//
+//    if (!$api->hasAccessToken() && !$api->requestTokenWithAuthCode()) {
+//    // No valid access token available, go to authorization server
+//        header('Location: ' . $api->getAuthorizeUri());
+//        exit;
+//    }
+    return true;
 }
 
 $app->match('/php_sdk/oauth.php', function (Request $request) use ($app) {
